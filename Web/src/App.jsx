9069200fef56027385283c9d35791960e6004d74
Bad Sky <charlesdelimaroberto@gmail.com>
2022-07-15 10:57:54 -0300

    first commit

---
+++ Web/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <>
+    <h1>Hello Word</h1>
+    <img src="#" alt="#" />
+    <p>Inciando projeto</p>
+    </>
+  )
+}
+
+export default App
